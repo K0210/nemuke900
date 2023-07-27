8cf71c565c6d2bcfb90b6792a0ca595eb64c21c1
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 11:21:22 +0900

    Create wandahoi.js

---
+++ wandahoi.js
@@ -0,0 +1,10 @@
+var wanwan = wanwan(){
+let emucha = document.getElementById('memo').value;
+console.log(emucha)
+if (emucha == ''){
+emu = 'ほへっ？';
+}else{
+emu = null;
+};
+document.getElementById('emu').innerHTML = emu ;
+};
